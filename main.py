7fc2efcc4b1dff3f44215e97538ccba02734a696
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2024-02-20 17:48:00 -0300

    Versão 0.3
    Adicionei o banco de dados local e a comunicação com ele. Versão já funcional.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 from string import ascii_letters
+from sqlite3 import connect
 
 
 def swap(c):
     try:
         del(app)
+        print("Olá")
     except:
         pass
     finally:
@@ -12,7 +14,26 @@ def swap(c):
             app = Login()
         elif c.lower() == "signin":
             app = SignIn()
+        elif c.lower() == "final":
+            app = FinalPage()
 
+class FinalPage:
+    def __init__(self):
+        self.janela = tk.Tk()
+        self.janela.title("FinalPage")
+        self.x = (self.janela.winfo_screenwidth() / 2) - (700 / 2)
+        self.y = (self.janela.winfo_screenheight() / 2) - (600 / 2)
+        self.janela.geometry(f"700x600+{int(self.x)}+{int(self.y)}")
+        self.frame = tk.Frame(self.janela, width=700, height=600, background="#3A4570")
+        self.frame.place(x=0, y=0)
+        self.label = tk.Label(master=self.frame,
+                              font="Inter 30",
+                              bg="#3A4570",
+                              fg="#F0B305",
+                              text="Você fez login com sucesso!")
+        self.label.place(x=20, y=20)
+
+        self.janela.mainloop()
 
 class Login:
     def __init__(self):
@@ -70,7 +91,6 @@ class Login:
                                   font="Inter 10")
         self.EntryUser.place(x=75, y=140)
         self.EntryUser.placeholder = "Nome de usuário ou email"
-        self.EntryUser.name = "EntryUser"
         self.EntryUser.insert(0, "Nome de usuário ou email")
         self.EntryUser.bind("<FocusIn>",
                             lambda event, entry=self.EntryUser:
@@ -96,7 +116,6 @@ class Login:
                                   font="Inter 10")
         self.EntryPass.place(x=75, y=215)
         self.EntryPass.placeholder = "Senha"
-        self.EntryPass.name = "EntryPass"
         self.EntryPass.insert(0, "Senha")
         self.EntryPass.bind("<FocusIn>",
                             lambda event, entry=self.EntryPass:
@@ -116,8 +135,7 @@ class Login:
                                      borderwidth=0,
                                      highlightthickness=0)
         self.LoginButton.place(x=160, y=309)
-        self.LoginButton.bind("<Button-1>", lambda _, args={"UserEmail": self.EntryUser, "Pass": self.EntryPass}:
-        self.white_entry(args))
+        self.LoginButton.bind("<Button-1>", self.white_entry)
         self.LoginButton.bind("<ButtonPress-1>", lambda event, button=self.LoginButton:
         self._on_pressed(button), add=True)
         self.LoginButton.bind("<ButtonRelease-1>", lambda event, button=self.LoginButton:
@@ -153,7 +171,9 @@ class Login:
         self.janela.destroy()
         swap("SignIn")
 
-    def white_entry(self, args):
+    def white_entry(self, event):
+        args = {"UserEmail": self.EntryUser,
+                "Pass": self.EntryPass}
         for entry in args.values():
             if entry.get() == "" or entry.get() == " ":
                 entry.Label['text'] = "*Não pode estar vazio"
@@ -162,7 +182,33 @@ class Login:
                 entry.Label['text'] = "*Não pode estar vazio"
                 break
         else:
-            pass
+            state = 0
+            email_or_user = 0
+            user_email = args['UserEmail'].get()
+            password = args['Pass'].get()
+            state += 1 if self.check_database(args['Pass'].get(), what_check="password") else 0
+            if args['UserEmail'].get().endswith("@gmail.com"):
+                state += 1 if self.check_database(args['UserEmail'].get()) else 0
+                email_or_user = 0
+            else:
+                state += 1 if self.check_database(args['UserEmail'].get(), what_check="name") else 0
+                email_or_user = 1
+            if state == 2:
+                self.janela.destroy()
+                swap("final")
+                return "break"
+
+
+    def check_database(self, for_check, what_check='email'):
+        for_check = for_check.lower() if what_check == "email" else for_check
+        con = connect("data.db")
+        exe = con.cursor()
+        select = exe.execute("select * from users where ¨ = ?".replace("¨", what_check), (for_check, ))
+        if select.fetchall():
+            con.close()
+            return True
+        con.close()
+        return False
 
 
 class SignIn:
@@ -315,11 +361,7 @@ class SignIn:
                                       borderwidth=0,
                                       highlightthickness=0)
         self.SignInButton.place(x=150, y=459)
-        self.SignInButton.bind("<Button-1>", lambda _, args={"User": self.EntryUser,
-                                                             "Email": self.EntryEmail,
-                                                             "Pass": self.EntryPass,
-                                                             "RPass": self.EntryPassR}:
-        self.white_entry(args))
+        self.SignInButton.bind("<Button-1>", self.white_entry)
         self.SignInButton.bind("<ButtonPress-1>", lambda event, button=self.SignInButton:
         self._on_pressed(button), add=True)
         self.SignInButton.bind("<ButtonRelease-1>", lambda event, button=self.SignInButton:
@@ -355,9 +397,15 @@ class SignIn:
         self.janela.destroy()
         swap("Login")
 
-    def white_entry(self, args):
-        for entry in args.values():
-            if entry.get() == "" or entry.get() == " ":
+    def white_entry(self, event):
+        args = {"User": self.EntryUser,
+                "Email": self.EntryEmail,
+                "Pass": self.EntryPass,
+                "PassR": self.EntryPassR}
+        for index, entry in args.items():
+            if index == "User":
+                pass
+            elif entry.get() == "" or entry.get() == " ":
                 entry.Label['text'] = "*Não pode estar vazio"
                 break
             elif entry.get() == entry.placeholder:
@@ -365,80 +413,119 @@ class SignIn:
                 break
         else:
             self.check_entry(args)
+            return "break"
 
     def check_entry(self, args):
         for index, entry in args.items():
+            entry.Label['text'] = ""
             if index == "UserEmail":
                 # Lógica Ou Ou em python
                 if ((self.check_name(entry) or not self.check_email(entry)) and
                         (not self.check_name(entry) or self.check_email(entry))):
-                    entry.Label['text'] = "*Nome ou email inválidos"
                     break
             elif index == "User":
                 if not self.check_name(entry):
-                    entry.Label['text'] = "*Nome de usuário inválido"
                     break
             elif index == "Email":
                 if not self.check_email(entry):
-                    entry.Label['text'] = "*email inválido"
                     break
             elif index == "Pass":
                 if not self.check_pass(entry):
-                    entry.Label['text'] = "*Senha deve conter 8 caracteres, simbólos, letras e números"
                     break
             elif index == "RPass":
                 if args['RPass'].get() != args['Pass'].get():
                     entry.Label['text'] = "*Senhas não coincidem"
                     break
         else:
-            pass
+            self.print_to_database(args['User'].get(), args['Email'].get(), args['Pass'].get())
+            self.janela.destroy()
+            swap("login")
 
     def check_name(self, entry):
         name = entry.get().strip()
         if " " in name:
+            entry.Label['text'] = "*Nome não pode conter espaços em branco"
+            return False
+        elif name == "":
+            return True
+        elif len(name) > 150:
+            entry.Label['text'] = "*Nome de usuário muito grande, MAX: 150"
             return False
         else:
             for i in name:
                 if i in ascii_letters or i.isdigit() or i == "_":
                     pass
                 else:
+                    entry.Label['text'] = "*Nome de usuário inválido"
                     return False
         return True
 
     def check_email(self, entry):
-        email = entry.get()
+        email = entry.get().strip()
         adress = email.lower().find("@gmail.com")
         if adress == -1:
+            entry.Label['text'] = "*Email deve conter @gmail.com"
+            return False
+        elif len(email) > 150:
+            entry.Label['text'] = "*Email muito grande, MAX: 150"
+            return False
+        elif self.check_database(email):
+            entry.Label['text'] = "*Email já cadastrado"
             return False
         else:
             for i in email[:adress]:
                 if i.isalpha() or i.isdigit or i == ".":
                     pass
                 else:
+                    entry.Label['text'] = "*Email inválido"
                     return False
         return True
 
     def check_pass(self, entry):
-        password = entry.get()
+        password = entry.get().strip()
         digits = False
         alpha = False
         symbols = False
         if " " in password:
+            entry.Label['text'] = "*Senha não pode conter espaços"
             return False
         elif len(password) < 8:
+            entry.Label['text'] = "*Senha deve conter 8 caracteres, simbólos, letras e números"
+            return False
+        elif len(password) > 150:
+            entry.Label['text'] = "*Senha muito grande, MAX: 150"
             return False
         for i in password:
             if i.isdigit():
                 digits = True
             elif i.isalpha():
                 alpha = True
-            elif i in "!@#$%¨&*_-+*/.,;:|":
+            elif i in "!@#$%¨&*_-+*/.,;:|\\":
                 symbols = True
             else:
+                entry.Label['text'] = "*Caractere não reconhecido"
                 return False
         if not (digits and alpha and symbols):
+            entry.Label['text'] = "*Senha deve conter 8 caracteres, simbólos, letras e números"
             return False
         return True
+
+    def check_database(self, for_check, what_check='email'):
+        for_check = for_check.lower() if what_check == "email" else for_check
+        con = connect("data.db")
+        exe = con.cursor()
+        select = exe.execute("select * from users where ¨ = ?".replace("¨", what_check), (for_check,))
+        if select.fetchall():
+            return True
+        return False
+
+    def print_to_database(self, name, email, password):
+        con = connect("data.db")
+        exe = con.cursor()
+        exe.execute("insert into users (name, email, password) values (?, ?, ?)",
+                    (name, email.lower(), password))
+        con.commit()
+        con.close()
 
 
 if __name__ == "__main__":
